092194d1a16469b4aae1cce066c69c34f0b1d8e0
arzkindx <anggitaardy@gmail.com>
2023-12-14 01:16:31 +0700

    Files upload

---
+++ application/views/pages/about.php
@@ -0,0 +1,16 @@
+    <!-- the section start here  -->
+    <section class="about">
+        <div class="about_us">
+            <h2>Tentang Kami</h2>
+            <p>The Ardy Bakery adalah toko kue bergaya Eropa yang baru berkembang saat ini di Indonesia. Setiap Kuenya dibuat dengan cinta dan hasrat untuk cita rasa terbaik,</p>
+            <p>Didirikan pada tahun 2023, Ardy Bakery adalah salah satu pelopor rantai toko kue bergaya Eropa di Indonesia. Dengan lebih dari 10 toko dan berkembang 
+                di kota-kota besar dan kecil di Indonesia, Ardy Bakery terus menjanjikan kualitas dan layanan yang baik kepada para pelanggan setia pada umumnya.</p>
+            <p>Ardy Bakery memberikan jaminan kualitas produk yang unggul, presentasi produk Sensasional dengan kemasan inventif dan standar layanan yang luar biasa. 
+                Pendekatan inovatif Ardy Bakery mendorong untuk terus meneliti dan mengembangkan produk, menambahkan kolaborasi seni di setiap kreasi, menjadikannya pemimpin pasar dalam bisnis ini.</p>
+            
+        </div>
+        <div class="home-btn">
+            <a href="<?= base_url()?>"><button>Home</button></a>
+        </div>
+    </section>
+    <!-- the section end here  -->
